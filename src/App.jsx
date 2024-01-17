fcbaa637a3464821dd2c5d567d11d28de8359991
Shivam <guptashivam9893@gmail.com>
2024-01-17 10:32:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/styles.scss'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
