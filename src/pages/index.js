309b30b3c537882d2a4ead5bb92922d35972c8dd
Shivam <guptashivam9893@gmail.com>
2024-01-17 15:39:42 +0530

    created pages and components

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Testimonial from './Testimonial'
+import Team from './Team'
+import Models from './Models'
+
+
+export { Home, About, Contact, Testimonial, Team, Models }
