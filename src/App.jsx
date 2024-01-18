644b82c34267baaff589a18845a5475e246e63d9
Shivam <guptashivam9893@gmail.com>
2024-01-18 20:39:04 +0530

    set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './styles/styles.scss'
 import { Route, Routes } from 'react-router-dom'
 import { Navbar } from './components'
-import { Home } from './pages'
+import { About, Contact, Home, Models, Team, Testimonial } from './pages'
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route index='/' element={<Home />} />
+        <Route index path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/models' element={<Models />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/testimonials' element={<Testimonial />} />
       </Routes>
     </>
   )
