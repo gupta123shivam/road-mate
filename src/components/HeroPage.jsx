fd1053ddf67d7cd548a4871bb2faffe492bcfb3e
Shivam <guptashivam9893@gmail.com>
2024-02-04 19:20:47 +0530

    completed HeroPage

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -1,8 +1,21 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom'
 
-
-const HeroPage = () => {
+function HeroPage({ name }) {
   return (
-    <div>HeroPage</div>
+    <>
+      <section className='hero-pages'>
+        <div className='hero-pages__overlay'></div>
+        <div className='container'>
+          <div className='hero-pages__text'>
+            <h3>{name}</h3>
+            <p>
+              <Link to='/'>Home</Link> / {name}
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
