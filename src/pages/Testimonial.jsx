ee0235c09638ed04fb038f64901698eaf0c971f4
Shivam <guptashivam9893@gmail.com>
2024-02-06 20:25:39 +0530

    completed Testimonials Page

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { IconPhone } from '@tabler/icons-react'
+import { HeroPage, Testimonials } from '../components'
 
-const Testimonial = () => {
+function TestimonialsPage() {
   return (
-    <div>Testimonial</div>
+    <>
+      <section className='testimonial-page'>
+        <HeroPage name='Testimonials' />
+        <Testimonials />
+        <div className='book-banner'>
+          <div className='book-banner__overlay'></div>
+          <div className='container'>
+            <div className='text-content'>
+              <h2>Book a car by getting in touch with us</h2>
+              <span>
+                <IconPhone width={40} height={40} />
+                <h3>(123) 456-7869</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
-export default Testimonial
+export default TestimonialsPage
